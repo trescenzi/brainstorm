966df38a19f2f79cbab45e4b1f60477b3f4850b3
Thomas Crescenzi <trescenzi@gmail.com>
2021-01-19 18:06:01 -0500

    get card names at build time

---
+++ processCardNames.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const unique = require('lodash').uniq;
+const fetch = require('node-fetch');
+
+async function main() {
+  console.log('Grabbing card names from mtgjson');
+  const allPrintings = await (await fetch('https://mtgjson.com/api/v5/AllPrintings.json')).json();
+  console.log(`Got ${Object.keys(allPrintings.data)} sets`);
+  const allNames = unique(Object.values(allPrintings.data).reduce((all, {cards}) => [...all, ...cards.map(({name}) => name)], []));
+  console.log(`Found ${allNames.length} names`);
+  fs.writeFileSync('./allnames.json', JSON.stringify(allNames));
+}
+
+main();
