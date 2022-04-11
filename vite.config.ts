f998cf20175b408eea10f2c8e7669cf4a08fafd4
Thomas Crescenzi <trescenzi@gmail.com>
2022-04-11 16:51:52 -0400

    fix: output to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  build: {
+    outDir: 'build',
+  }
 })
