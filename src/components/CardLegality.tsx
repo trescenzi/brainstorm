4ab4a55a217e03d003ff8081a55480eedfa856a8
Thomas Crescenzi <trescenzi@gmail.com>
2022-04-29 22:35:01 -0400

    feat(Spacing): improve small desktop sizing

--- src/components/CardLegality.tsx
+++ src/components/CardLegality.tsx
@@ -7,6 +7,9 @@ function CardLegality({format, legality}: {format: string, legality: TCardLegali
 
 export function CardLegalities({card}: {card: ScryFallCard}) {
   return <div class={classes.legalities}>
+    <div class={classes.legalitiesHeader}>
+      <h2><i>{card.name}</i> legality across formats:</h2>
+    </div>
     <CardLegality format="commander" legality={card.legalities.commander} />
     <CardLegality format="modern" legality={card.legalities.modern} />
     <CardLegality format="legacy" legality={card.legalities.legacy} />
@@ -15,5 +18,21 @@ export function CardLegalities({card}: {card: ScryFallCard}) {
     <CardLegality format="standard" legality={card.legalities.standard} />
     <CardLegality format="explorer" legality={card.legalities.standard} />
     <CardLegality format="historic" legality={card.legalities.standard} />
+    <div class={classes.legalityKey}>
+      <div class={classes.legal} />
+      <span>Legal</span>
+    </div>
+    <div class={classes.legalityKey}>
+      <div class={classes.not_legal} />
+      <span>Not Legal</span>
+    </div>
+    <div class={classes.legalityKey}>
+      <div class={classes.banned } />
+      <span>Banned</span>
+    </div>
+    <div class={classes.legalityKey}>
+      <div class={classes.restricted} />
+      <span>Restricted</span>
+    </div>
   </div>
 }
