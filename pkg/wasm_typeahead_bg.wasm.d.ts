2265533e653da6d335a5d4d6ebdf3d5a2c5bff99
Thomas Crescenzi <trescenzi@gmail.com>
2023-09-14 09:50:13 -0400

    ship built rust files

---
+++ pkg/wasm_typeahead_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_typeahead_free(a: number): void;
+export function typeahead_new(a: number, b: number): number;
+export function typeahead_search(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
